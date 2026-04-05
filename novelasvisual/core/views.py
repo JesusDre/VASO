66ca170ac96e2ddc4c460712083e78952b829e26
Jesus <20233tn229@utez.edu.mx>
2026-04-04 19:27:04 -0600

    [ADD] Creacion de views para el index

--- novelasvisual/core/views.py
+++ novelasvisual/core/views.py
@@ -1,3 +1,6 @@
+# Vista de inicio del proyecto
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    # Renderiza la pagina de inicio
+    return render(request, 'index.html')
