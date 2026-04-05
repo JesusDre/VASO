5fd80f7221c4ed2c752e6a16504e023964cbe712
Jesus <20233tn229@utez.edu.mx>
2026-04-04 19:17:24 -0600

    [ADD] configuración global de settings y urls novelavisual

--- novelasvisual/novelasvisual/urls.py
+++ novelasvisual/novelasvisual/urls.py
@@ -1,22 +1,35 @@
-"""
-URL configuration for novelasvisual project.
+# URL raiz del proyecto Novelas Visuales
+# Incluye las rutas de todas las apps y los endpoints de JWT
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from core import views as core_views
+from usuarios.views import RegistroView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Vista de inicio (renderiza template HTML principal)
+    path('', core_views.index, name='index'),
+
+    # Endpoint para iniciar sesion: recibe email y password, devuelve access y refresh tokens
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    # Endpoint para refrescar el access token usando el refresh token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Endpoint de registro publico (AllowAny)
+    path('api/registro/', RegistroView.as_view(), name='registro'),
+
+    # Rutas de cada app del proyecto
+    path('', include('usuarios.urls')),
+    path('', include('recursos.urls')),
+    path('', include('historias.urls')),
+    path('', include('nodos.urls')),
+    path('', include('personajes.urls')),
+    path('', include('progreso.urls')),
 ]
+
+# Servir archivos de medios en modo DEBUG
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
