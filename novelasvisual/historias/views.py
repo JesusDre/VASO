0b2baf1d7c4d88bb34ed9ed1835ac59b567b5a3d
Tomas <20233tn147@utez.edu.mx>
2026-04-04 19:36:16 -0600

    [update] se modifico la aplicacion historias agregando serializer y urls y modificando el contenido de models y views.

--- novelasvisual/historias/views.py
+++ novelasvisual/historias/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# Vistas de la app historias
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Historia
+from .serializers import HistoriaSerializer
 
-# Create your views here.
+
+# -----------------------------------------------------------
+# ViewSet de Historia: lectura publica, escritura requiere auth
+# -----------------------------------------------------------
+class HistoriaViewSet(viewsets.ModelViewSet):
+    queryset = Historia.objects.all()
+    serializer_class = HistoriaSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
