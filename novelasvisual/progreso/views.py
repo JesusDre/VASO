b729d2a8cea185ecb7cf22437e19d7c71cf91ce2
Jesus <20233tn229@utez.edu.mx>
2026-04-04 20:02:27 -0600

    [UPD] modificacion de models y views [ADD] agregación de serializes y urls

--- novelasvisual/progreso/views.py
+++ novelasvisual/progreso/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# Vistas de la app progreso
+# El ViewSet filtra los progresos por el usuario autenticado actual
+from rest_framework import viewsets
+from .models import ProgresoUsuario
+from .serializers import ProgresoUsuarioSerializer
 
-# Create your views here.
+
+# -----------------------------------------------------------
+# ViewSet de ProgresoUsuario
+# Solo devuelve los progresos del usuario que hace la peticion
+# -----------------------------------------------------------
+class ProgresoUsuarioViewSet(viewsets.ModelViewSet):
+    serializer_class = ProgresoUsuarioSerializer
+
+    def get_queryset(self):
+        # Filtramos para que cada usuario solo vea su propio progreso
+        return ProgresoUsuario.objects.filter(id_usuario=self.request.user)
