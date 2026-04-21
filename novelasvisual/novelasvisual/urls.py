43c2763a338d12ca84932b17d33d945dfba00eac
Jesus <20233tn229@utez.edu.mx>
2026-04-20 19:44:36 -0600

    [FIX] ulrs.py de novelasvisual para media

--- novelasvisual/novelasvisual/urls.py
+++ novelasvisual/novelasvisual/urls.py
@@ -1,9 +1,10 @@
 # URL raiz del proyecto Novelas Visuales
 # Incluye las rutas de todas las apps y los endpoints de JWT
 
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core import views as core_views
 from usuarios.views import RegistroView
@@ -33,4 +34,7 @@ urlpatterns = [
 ]
 
 # Servir archivos de medios (DEBUG y produccion)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
